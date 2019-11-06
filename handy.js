e5c08ef0f3fbd158d480a074bf2ecdd6892d0f23
RickSnijders <99060582@mydavinci.nl>
2019-11-06 14:26:51 +0100

    klaar

--- handy.js
+++ handy.js
@@ -5,6 +5,13 @@ function openMenu(){
 function closeMenu(){
 	document.getElementById("klapMenu").style.display = "none";
 	document.getElementById("button").setAttribute('onclick', 'openMenu()');
+}
 
-
+function openMenuDiensten(){
+	document.getElementById("klapMenuDiensten").style.display = "block";
+	document.getElementById("button").setAttribute('onclick', 'closeMenuDiensten()');
+}
+function closeMenuDiensten(){
+	document.getElementById("klapMenuDiensten").style.display = "none";
+	document.getElementById("button").setAttribute('onclick', 'openMenuDiensten()');
 }
